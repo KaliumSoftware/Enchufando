b79a791c1991dec32002d6f5b38edcd2d66129b1
Joshua Candia <joshuacandia74@gmail.com>
2023-10-03 07:09:51 -0300

    feat(Server/Models): Models ceated

---
+++ server/src/models/User.js
@@ -0,0 +1,33 @@
+import { DataTypes, Model } from 'sequelize';
+
+export function defineSpellsModel(sequelize) {
+  class User extends Model {
+    static associate(models) {
+      // Definir asociaciones aquí
+      User.belongsToMany(models.Order, {
+        through: 'OrderUser',
+        timestamps: false
+      });
+    }
+  }
+
+  User.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    { sequelize, modelName: 'User' }
+  );
+  return User;
+}
