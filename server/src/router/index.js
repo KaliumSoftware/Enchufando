f7084364de4f78f3539eed32c136cf24db15207d
DiazAdriel0 <diazadriel0@gmail.com>
2023-10-04 12:41:38 -0300

    add(server): add index file to folders

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
+const {
+  orderRouter,
+  productRouter,
+  userRouter
+} = require('./routes');
+
 const router = Router();
-const orderRouter = require('./routes/orderRouter');
-const productRouter = require('./routes/productRouter');
-const userRouter = require('./routes/userRouter');
+
 router.use('/api/order', orderRouter);
+
 router.use('/api/product', productRouter);
+
 router.use('/api/user', userRouter);
 
 module.exports = router;
