f798e94116b95adb85341acf1a424604d1bc0e12
Joshua Candia <joshuacandia74@gmail.com>
2023-10-04 19:20:33 -0300

    feat(Server/Handlers): index updated with new handler

--- server/src/handlers/index.js
+++ server/src/handlers/index.js
@@ -1,11 +1,13 @@
-//read
+//users
 const getAllUsers = require('../handlers/userHandlers/readUsers/getAllUsers');
 const getUserById = require('../handlers/userHandlers/readUsers/getUserById');
-//create
 const createUser = require('../handlers/userHandlers/createUsers/createUser');
+//orders
+const getAllOrders = require('../handlers/orderHandlers/readOrders/getAllOrders');
 
 module.exports = {
   getAllUsers,
   createUser,
-  getUserById
+  getUserById,
+  getAllOrders
 };
