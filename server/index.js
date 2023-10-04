511ded2c06066bb4148f02302199ecf842b877a1
DiazAdriel0 <diazadriel0@gmail.com>
2023-10-04 08:04:10 -0300

    fix(server): normalize port

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
-const server = require('./src/app');
+const express = require('express');
+const next = require('next');
 const { sequelize } = require('./src/db');
-const PORT = 3000;
 
-// Syncing all the models at once.
-sequelize.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`server raised on port: ${PORT}`);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Aquí puedes agregar tus rutas y lógica de Express
+  server.get('/api/someendpoint', (req, res) => {
+    res.json({ message: 'This is an API endpoint' });
+  });
+
+  // Configura Next.js para manejar todas las demás rutas
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  const PORT = process.env.PORT || 3000;
+
+  sequelize.sync({ force: false }).then(() => {
+    server.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+    });
   });
 });
