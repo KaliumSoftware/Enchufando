27229db62fe69e6604d40674201d0086da47b3f1
Joshua Candia <joshuacandia74@gmail.com>
2023-10-04 19:19:26 -0300

    feat(Server:controllers): index controller added

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,9 +1,13 @@
+//users handlers
 const findAllUsers = require('./userControllers/readUsers/findAllUsers');
 const createUserContr = require('./userControllers/createUsers/createUserContr');
 const findUserById = require('./userControllers/readUsers/findUserById');
+//orders handlers
+const findAllOrders = require('./orderControllers/readOrders/findAllOrders');
 
 module.exports = {
   findAllUsers,
   createUserContr,
-  findUserById
+  findUserById,
+  findAllOrders
 };
