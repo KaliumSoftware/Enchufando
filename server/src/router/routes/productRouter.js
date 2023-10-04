dbabf50cf18b14b7bd94b0583283af15a99d3b9d
DiazAdriel0 <diazadriel0@gmail.com>
2023-10-04 12:41:09 -0300

    iden(server/routers): code identation

--- server/src/router/routes/productRouter.js
+++ server/src/router/routes/productRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+
 const productRouter = Router();
 
 module.exports = productRouter;
--- server/src/router/routes/userRouter.js
+++ server/src/router/routes/userRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+
 const userRouter = Router();
 
 module.exports = userRouter;
