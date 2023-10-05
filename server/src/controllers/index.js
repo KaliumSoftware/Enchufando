94632bc27912774c3136128f0de1dbd8c695ba68
Joshua Candia <joshuacandia74@gmail.com>
2023-10-05 16:31:32 -0300

    feat(Server): handlers and controllers added

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -5,11 +5,16 @@ const findUserById = require('./userControllers/readUsers/findUserById');
 //orders
 const findAllOrders = require('./orderControllers/readOrders/findAllOrders');
 //products
+const findAllProducts = require('./productControllers/readProducts/findAllProducts');
+const findProductByPk = require('./productControllers/readProducts/findProductByPk');
 const createProduct = require('./productControllers/createProducts/createProduct');
+
 module.exports = {
   findAllUsers,
   createUserContr,
   findUserById,
   findAllOrders,
-  createProduct
+  createProduct,
+  findAllProducts,
+  findProductByPk
 };
