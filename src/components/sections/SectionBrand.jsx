edb209b6cea06c39a840346c05c7b9e8b5ae329a
DiazAdriel0 <diazadriel0@gmail.com>
2023-10-06 18:40:04 -0300

    feat(components/sections): SectionProducts added to home

--- src/components/sections/SectionBrand.jsx
+++ src/components/sections/SectionBrand.jsx
@@ -6,30 +6,31 @@ const SectionBrand = () => {
       <div className='mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center'>
         <div className='mx-auto max-w-xl text-center'>
           <h1 className='text-3xl font-extrabold sm:text-5xl'>
-            Understand User Flow.
-            <strong className='font-extrabold text-red-700 sm:block'>
-              Increase Conversion.
+            Calidad que Fluye Con Vos
+            <strong className='font-extrabold text-blueMain sm:block'>
+              Mejora la Eficiencia
             </strong>
           </h1>
 
           <p className='mt-4 sm:text-xl/relaxed'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Nesciunt illo tenetur fuga ducimus numquam ea!
+            Descubrí nuestra gama de accesorios de vanguardia para
+            conexiones de tuberías de agua y mangueras de riego.
+            ¡Optimizá tu sistema hoy mismo!
           </p>
 
           <div className='mt-8 flex flex-wrap justify-center gap-4'>
             <a
-              className='block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto'
+              className='block w-full rounded bg-blueMain px-12 py-3 text-sm font-medium text-white shadow hover:bg-blueDark focus:outline-none focus:ring active:bg-blueMain sm:w-auto'
               href='#'
             >
-              Get Started
+              Contactanos
             </a>
 
             <a
-              className='block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto'
+              className='block w-full rounded px-12 py-3 text-sm font-medium text-blueMain shadow hover:text-blueDark focus:outline-none focus:ring active:text-blueMain sm:w-auto'
               href='#'
             >
-              Learn More
+              Sobre Nosotros
             </a>
           </div>
         </div>
