7b7e4d4f39405f935dc0c17fc1e55d2c8086e29d
Valen <valenzalazar3@gmail.com>
2023-10-09 15:59:33 -0300

    feat(app/organisms): footer

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,7 +1,29 @@
 export default function Footer() {
   return (
-    <div className='h-20 w-full bg-gray-300 text-black'>
-      <h1 className='text-3xl'>Footer</h1>
-    </div>
+
+    <footer class="bg-gray-400 rounded-lg shadow dark:bg-gray-900 m-4">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+            <span class="self-center text-white text-2xl font-semibold whitespace-nowrap dark:text-white">Enchufando</span>
+          </a>
+          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0 dark:text-gray-400">
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">Facebook</a>
+            </li>
+            <li>
+              <a href="#" class="hover:underline">Contactanos</a>
+            </li>
+          </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-white sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Kalium Software</a>. Todos los derechos reservados.</span>
+      </div>
+    </footer>
+
+
   );
 }
