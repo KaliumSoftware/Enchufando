c0f7bc863b90b14a94188131d1d1b0a709c12884
Valen <valenzalazar3@gmail.com>
2023-10-09 17:59:30 -0300

    feat(app/organisms): footer

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,26 +1,26 @@
 export default function Footer() {
   return (
 
-    <footer class="bg-gray-100 rounded-lg shadow dark:bg-gray-900 m-4">
-      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
-            <span class="self-center  text-2xl font-semibold whitespace-nowrap dark:text-white">Enchufando</span>
+    <footer className="bg-gray-100 rounded-lg shadow dark:bg-gray-900">
+      <div className="w-full max-w-screen-2xl mx-auto p-4 md:py-14">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
+            <span className="self-center  text-4xl font-semibold whitespace-nowrap dark:text-white">Enchufando</span>
           </a>
-          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium  sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center mb-6 text-lg font-medium  sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">Facebook</a>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Facebook</a>
             </li>
             <li>
-              <a href="#" class="hover:underline">Contactanos</a>
+              <a href="#" className="hover:underline">Contactanos</a>
             </li>
           </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm  sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Kalium Software</a>. Todos los derechos reservados.</span>
+        <hr className="my-6 border-black sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm  sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Kalium Software</a>. Todos los derechos reservados.</span>
       </div>
     </footer>
 
