73f44fb7b9437de3aa0ff1153e68a3e74092a870
Valen <valenzalazar3@gmail.com>
2023-10-10 14:57:17 -0300

    fix(shoppingCart): show and center

--- src/app/store/page.jsx
+++ src/app/store/page.jsx
@@ -1,9 +1,11 @@
 import Products from '@/components/organisms/Products';
+import ShoppingCart from '@/components/organisms/ShoppingCart';
 
 export default function Store() {
   return (
     <div>
       <Products />
+
     </div>
   );
 }
