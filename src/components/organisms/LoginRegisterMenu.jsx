f65dcbc8655877ca25eac6cffc7551b1746e403d
DiazAdriel0 <diazadriel0@gmail.com>
2023-10-16 08:51:26 -0300

    feat(organisms/LoginRegister): form styled

--- src/components/organisms/LoginRegisterMenu.jsx
+++ src/components/organisms/LoginRegisterMenu.jsx
@@ -3,14 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 function LoginRegisterMenu({
-  login,
   setShowLoginMenu,
   signingin,
   setSigningin
 }) {
-  const [signIn, toggle] = useState(login);
   const [access, setAccess] = useState(false); //eslint-disable-line
-  const [input, setInput] = useState({
+  const [login, setLogin] = useState({
     email: '',
     password: ''
   });
@@ -25,10 +23,13 @@ function LoginRegisterMenu({
     address: '',
     code: ''
   });
-
-  useEffect(() => {
-    console.log(signingin);
-  }, []);
+  const [errorsSignUp, setErrorsSignUp] = useState({
+    name: '',
+    email: '',
+    password: '',
+    address: '',
+    code: ''
+  });
 
   const loggedUser = useSelector((state) => state.loggedUser);
 
@@ -46,9 +47,9 @@ function LoginRegisterMenu({
     }
   }, [access]);
 
-  const handleChange = (event) => {
-    setInput({
-      ...input,
+  const handleLoginChange = (event) => {
+    setLogin({
+      ...login,
       [event.target.name]: event.target.value
     });
   };
@@ -157,7 +158,7 @@ function LoginRegisterMenu({
   return (
     <div className='fixed inset-0 flex items-center justify-center h-full w-full'>
       <div className='fixed mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 z-20'>
-        <div className='mx-auto max-w-lg bg-white z-10 rounded-2xl py-16 px-32'>
+        <div className='mx-auto bg-white z-10 rounded-2xl py-16 px-32 w-[75vw] h-[75vh]'>
           <h1 className='text-center text-2xl font-bold text-indigo-600 sm:text-3xl'>
             {signingin ? 'Iniciar Sesión' : 'Registrarse'}
           </h1>
@@ -176,78 +177,304 @@ function LoginRegisterMenu({
                 : 'Registrate como usuario'}
             </p>
 
-            <div>
-              <label
-                for='email'
-                className='sr-only'
-              >
-                Email
-              </label>
-
-              <div className='relative'>
-                <input
-                  type='email'
-                  className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
-                  placeholder='Email'
-                />
-
-                <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-4 w-4 text-gray-400'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
+            <div className='flex items-center justify-evenly w-full'>
+              {signingin ? (
+                <div>
+                  <label
+                    for='email'
+                    className='sr-only'
                   >
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207'
+                    Email
+                  </label>
+
+                  <div className='relative'>
+                    <input
+                      type='email'
+                      className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                      placeholder='Email'
                     />
-                  </svg>
-                </span>
-              </div>
+
+                    <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-4 w-4 text-gray-400'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        <path
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          stroke-width='2'
+                          d='M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207'
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <label
+                    for='name'
+                    className='sr-only'
+                  >
+                    Nombre completo
+                  </label>
+
+                  <div className='relative'>
+                    <input
+                      type='text'
+                      className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                      placeholder='Nombre completo'
+                    />
+
+                    <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='icon icon-tabler icon-tabler-user-filled h-4 w-4 text-gray-400'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        stroke-width='2'
+                        stroke='currentColor'
+                        fill='none'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                      >
+                        <path
+                          stroke='none'
+                          d='M0 0h24v24H0z'
+                          fill='none'
+                        ></path>
+                        <path
+                          d='M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z'
+                          stroke-width='0'
+                          fill='currentColor'
+                        ></path>
+                        <path
+                          d='M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z'
+                          stroke-width='0'
+                          fill='currentColor'
+                        ></path>
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              )}
+
+              {!signingin && (
+                <div>
+                  <label
+                    for='email'
+                    className='sr-only'
+                  >
+                    Email
+                  </label>
+
+                  <div className='relative'>
+                    <input
+                      type='email'
+                      className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                      placeholder='Email'
+                    />
+
+                    <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-4 w-4 text-gray-400'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        <path
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          stroke-width='2'
+                          d='M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207'
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              )}
             </div>
 
-            <div>
-              <label
-                for='contraseña'
-                className='sr-only'
-              >
-                Contraseña
-              </label>
+            <div className='flex items-center justify-evenly w-full'>
+              <div>
+                <label
+                  for='contraseña'
+                  className='sr-only'
+                >
+                  Contraseña
+                </label>
 
-              <div className='relative'>
-                <input
-                  type='password'
-                  className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
-                  placeholder='Contraseña'
-                />
+                <div className='relative'>
+                  <input
+                    type='password'
+                    className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                    placeholder='Contraseña'
+                  />
 
-                <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-4 w-4 text-gray-400'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                  >
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
-                    />
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
-                    />
-                  </svg>
-                </span>
+                  <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-4 w-4 text-gray-400'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        stroke-width='2'
+                        d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                      />
+                      <path
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        stroke-width='2'
+                        d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
+                      />
+                    </svg>
+                  </span>
+                </div>
               </div>
+
+              {!signingin && (
+                <div>
+                  <label
+                    for='contraseña'
+                    className='sr-only'
+                  >
+                    Repetir contraseña
+                  </label>
+
+                  <div className='relative'>
+                    <input
+                      type='password'
+                      className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                      placeholder='Repetir contraseña'
+                    />
+
+                    <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-4 w-4 text-gray-400'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        <path
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          stroke-width='2'
+                          d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                        />
+                        <path
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          stroke-width='2'
+                          d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div className='flex items-center justify-evenly w-full'>
+              {!signingin && (
+                <div>
+                  <label
+                    for='address'
+                    className='sr-only'
+                  >
+                    Dirección
+                  </label>
+
+                  <div className='relative'>
+                    <input
+                      type='text'
+                      className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                      placeholder='Dirección'
+                    />
+
+                    <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='icon icon-tabler icon-tabler-map-pin h-4 w-4 text-gray-400'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        stroke-width='2'
+                        stroke='currentColor'
+                        fill='none'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                      >
+                        <path
+                          stroke='none'
+                          d='M0 0h24v24H0z'
+                          fill='none'
+                        ></path>
+                        <path d='M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0'></path>
+                        <path d='M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z'></path>
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              )}
+
+              {!signingin && (
+                <div>
+                  <label
+                    for='code'
+                    className='sr-only'
+                  >
+                    Código
+                  </label>
+
+                  <div className='relative'>
+                    <input
+                      type='text'
+                      className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                      placeholder='Código'
+                    />
+
+                    <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='icon icon-tabler icon-tabler-user-filled h-4 w-4 text-gray-400'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        stroke-width='2'
+                        stroke='currentColor'
+                        fill='none'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                      >
+                        <path
+                          stroke='none'
+                          d='M0 0h24v24H0z'
+                          fill='none'
+                        ></path>
+                        <path
+                          d='M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z'
+                          stroke-width='0'
+                          fill='currentColor'
+                        ></path>
+                        <path
+                          d='M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z'
+                          stroke-width='0'
+                          fill='currentColor'
+                        ></path>
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              )}
             </div>
 
             <button
