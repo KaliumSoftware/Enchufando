20c908fd25f92ac9698b276de719b990ba670d2e
Valen <valenzalazar3@gmail.com>
2023-10-19 12:35:37 -0300

    fix(ShoppingCart): delete and select fixed

--- src/components/organisms/ShoppingCart.jsx
+++ src/components/organisms/ShoppingCart.jsx
@@ -1,22 +1,14 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import CartProduct from '../molecules/CartProduct';
 import { v4 as uuidv4 } from 'uuid';
+import AsidePrice from '../molecules/AsidePirce';
+
+
 function ShoppingCart() {
   const [show, setShow] = useState(false);
   const cartProducts = useSelector((state) => state.cart.cartProducts);
-
-
-
-
-  useEffect(() => {
-    console.log(cartProducts);
-  }, [cartProducts]);
-
-
-
-
 
   return (
     <>
@@ -73,56 +65,7 @@ function ShoppingCart() {
                 />
               ))}
             </div>
-            <div className=' md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full'>
-              <div className='flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto'>
-                <div>
-                  <p className='text-4xl font-black leading-9 text-gray-800'>
-                    Resumen
-                  </p>
-                  <div className='flex items-center justify-between pt-16'>
-                    <p className='text-base leading-none text-gray-800'>
-                      Sub Total
-                    </p>
-                    <p className='text-base leading-none text-gray-800'>
-                      $9,000
-                    </p>
-                  </div>
-                  <div className='flex items-center justify-between pt-5'>
-                    <p className='text-base leading-none text-gray-800'>
-                      Descuento
-                    </p>
-                    <p className='text-base leading-none text-gray-800'>
-                      $30
-                    </p>
-                  </div>
-                  <div className='flex items-center justify-between pt-5'>
-                    <p className='text-base leading-none text-gray-800'>
-                      Impuesto
-                    </p>
-                    <p className='text-base leading-none text-gray-800'>
-                      $35
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <div className='flex items-center pb-6 justify-between lg:pt-5 pt-20'>
-                    <p className='text-2xl leading-normal text-gray-800'>
-                      Total Final
-                    </p>
-                    <p className='text-2xl font-bold leading-normal text-right text-gray-800'>
-                      $10.000
-
-                    </p>
-                  </div>
-                  <button
-                    onClick={() => setShow(!show)}
-                    className='text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white'
-                  >
-                    Checkout
-                  </button>
-                </div>
-              </div>
-            </div>
+            <AsidePrice />
           </div>
         </div>
       </div>
