eca5199eef65439cb0bd9b5b202ac4d64c306bff
Valen <valenzalazar3@gmail.com>
2023-10-19 12:35:28 -0300

    fix(ShoppingCart): delete and select fixed

--- src/components/atoms/ButtonAdd.jsx
+++ src/components/atoms/ButtonAdd.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { addToCart } from '../../redux/slices/cartSlice';
-
+import { v4 as uuidv4 } from 'uuid';
 export default function ButtonAdd({ props }) {
 
     const dispatch = useDispatch();
     const handleAddCart = (props) => {
+        props = {
+            ...props,
+            localId: uuidv4()
+        }
         if (props) {
             return dispatch(addToCart(props));
         }
