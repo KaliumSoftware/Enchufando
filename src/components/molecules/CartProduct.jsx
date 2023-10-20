4f729c204acf35c1066eb21020b281850bd1bd36
Valen <valenzalazar3@gmail.com>
2023-10-20 07:08:43 -0300

    add(cartProduct): modularization

--- src/components/molecules/CartProduct.jsx
+++ src/components/molecules/CartProduct.jsx
@@ -1,53 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { Select, Option, Checkbox } from '@material-tailwind/react';
+import React from 'react'
 import TooltipReusable from '../atoms/TooltipReusable';
 import ButtonDelete from '../atoms/ButtonDelete';
 import ButtonAdd from '../atoms/ButtonAdd';
-import { useDispatch } from 'react-redux';
-import { setSpecificationsCart } from '@/redux/slices/cartSlice';
+import Quantity from '../atoms/Quantity';
+import Check from '../atoms/Check';
+import Measure from '../atoms/Measure';
 
 
 export default function CartProduct(props) {
     const { name, image, specifications, id, localId, selectedSpec } = props;
-    const dispatch = useDispatch()
-
-
-    const handleChange = (event) => {
-        const selectedProduct = specifications?.find(
-            (spec) => spec.size === event.target.value
-        );
-        if (selectedProduct) {
-            dispatch(setSpecificationsCart({
-                selectedSpec: selectedProduct,
-                localId
-            }))
-        }
-    };
-
-    const handleCheck = (event) => {
-        if (event.target.name === 'one') {
-            dispatch(setSpecificationsCart({
-                selectedSpec: { ...selectedSpec, pack: 'small' },
-                localId
-            }))
-        } else {
-            dispatch(setSpecificationsCart({
-                selectedSpec: { ...selectedSpec, pack: 'big' },
-                localId
-            }))
-        }
-    };
-
-    const handleQuantityChange = (event) => {
-        const { value } = event.target;
-        const numberRegex = /^[1-9]\d*$/;
-        if (numberRegex.test(value) || value.length === 0) {
-            dispatch(setSpecificationsCart({
-                selectedSpec: { ...selectedSpec, quantity: value },
-                localId
-            }))
-        }
-    };
 
     return (
         <div className='md:flex items-center py-8 border-t border-gray-200'>
@@ -62,110 +23,24 @@ export default function CartProduct(props) {
                 <p className='text-xl pb-6 leading-3 text-gray-800 md:pt-0 pt-4'>
                     {name}
                 </p>
-                <select
-                    label='Medidas'
-                    onChange={handleChange}
-                    value={selectedSpec?.size}
-                >
-                    {specifications?.map((spec) => (
-                        <option
-                            key={spec.code}
-                            value={spec.size}
-                        >
-                            {spec.size}
-                        </option>
-                    ))}
-                </select>
+                <Measure
+                    selectedSpec={selectedSpec}
+                    localId={localId}
+                    specifications={specifications} />
                 <TooltipReusable
                     text='Seleccione una medida'
                     show={false}
                     color='default'
                 >
-
-                    <div className='flex flex-wrap mx-5 pt-6 gap-2 pb-4 '>
-                        <div className='flex items-center'>
-                            <label htmlFor={`${id}checkOne`}>
-                                {selectedSpec ? `x ${selectedSpec?.smallPack}` : 'Bolsita'}
-                            </label>
-                            <Checkbox
-                                id={`${id}checkOne`}
-                                name='one'
-                                checked={selectedSpec?.pack === 'small'}
-                                onChange={handleCheck}
-                            />
-                        </div>
-                        <div className='flex items-center'>
-                            <label htmlFor={`${id}checkTwo`}>
-                                {selectedSpec ? `x ${selectedSpec?.bigPack}` : 'Bolsón'}
-                            </label>
-
-                            <Checkbox
-                                id={`${id}checkTwo`}
-                                name='two'
-                                checked={selectedSpec?.pack === 'big'}
-                                onChange={handleCheck}
-                            />
-                        </div>
-                    </div>
+                    <Check
+                        selectedSpec={selectedSpec}
+                        localId={localId}
+                        id={id} />
                 </TooltipReusable>
-                <div className='flex w-full flex-wrap md:flex-nowrap justify-evenly gap-2 items-center content-center'>
-                    <div className='w-2/6 m-2 my-4'>
-                        <label
-                            htmlFor={`${id}quantity`}
-                            className='relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-black focus-within:ring-1 focus-within:ring-black selection:'
-                        >
-                            <input
-                                type='number'
-                                id={`${id}quantity`}
-                                placeholder='Cantidad'
-                                className='peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm'
-                                onChange={handleQuantityChange}
-                                value={selectedSpec?.quantity}
-                            />
-
-                            <span className='absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs'>
-                                Cantidad
-                            </span>
-                        </label>
-                    </div>
-
-                    <div className='w-2/6'>
-                        <p
-                            className={`text-lg font-bold text-gray-500 dark:text-white ${
-                /* user.discount &&  */ 'line-through'
-                                }`}
-                        >
-                            $ {selectedSpec?.price}
-                        </p>
-                        <p className='text-lg font-bold text-gray-900 dark:text-white'>
-                            $
-                            {(selectedSpec?.pack === 'small' || selectedSpec?.pack === 'big') &&
-                                selectedSpec?.price /* && user.discount */ &&
-                                (selectedSpec?.price /* * user.discount */)
-                            }
-
-                        </p>
-                    </div>
-
-                    <div className='w-2/6'>
-                        <p className='text-lg font-bold text-gray-900 dark:text-white'>
-                            Total
-                        </p>
-                        <p className='text-lg font-bold text-gray-900 dark:text-white'>
-                            $
-                            {(selectedSpec?.pack === 'small' || selectedSpec?.pack === 'big') &&
-                                selectedSpec?.price &&
-                                selectedSpec?.quantity &&
-                                (
-                                    selectedSpec?.price *
-                                    (selectedSpec?.pack === 'small'
-                                        ? selectedSpec?.smallPack
-                                        : selectedSpec?.bigPack) /* * user.discount */ *
-                                    selectedSpec?.quantity
-                                )?.toFixed(2)}
-                        </p>
-                    </div>
-                </div>
+                <Quantity
+                    selectedSpec={selectedSpec}
+                    localId={localId}
+                    id={id} />
 
                 <div className='flex items-center justify-between pt-5 pr-6'>
                     <div className='flex itemms-center'>
@@ -174,7 +49,6 @@ export default function CartProduct(props) {
                         <ButtonDelete
                             localId={localId} />
                     </div>
-
                 </div>
             </div>
         </div >
