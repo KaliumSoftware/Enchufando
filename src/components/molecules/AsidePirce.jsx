48c36bb158cc899be23697aa74d30a627c123e3e
Valen <valenzalazar3@gmail.com>
2023-10-23 12:17:10 -0300

    add(AsidePrice): add total final logic

--- src/components/molecules/AsidePirce.jsx
+++ src/components/molecules/AsidePirce.jsx
@@ -1,6 +1,35 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function AsidePirce() {
+    const allInCart = useSelector(state => state.cart.cartProducts)
+    const userCode = useSelector(state => state.user.loggedUser.discount)
+
+    const subTotal = allInCart.map((product) => {
+        if (product.selectedSpec) {
+            if (product.selectedSpec.pack === 'big') {
+                const resultBig = product.selectedSpec.bigPack * product.selectedSpec.price * product.selectedSpec.quantity
+                return resultBig
+            } else {
+                const resultSmall = product.selectedSpec.smallPack * product.selectedSpec.price * product.selectedSpec.quantity
+                return resultSmall
+            }
+
+        }
+    }).reduce((a, b) => a + b, 0)
+
+    const totalFinal = subTotal * (userCode / 100)
+
+
+    const sendOrder = () => {
+        if (isNaN(totalFinal)) {
+            alert('Hay productos incompletos, por favor seleccione todas las caracteristicas')
+        } else {
+            alert('Gracias por su compra')
+        }
+    }
+
+
     return (
         <div className=' md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full'>
             <div className='flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto'>
@@ -13,7 +42,7 @@ export default function AsidePirce() {
                             Sub Total
                         </p>
                         <p className='text-base leading-none text-gray-800'>
-                            $9,000
+                            ${subTotal}
                         </p>
                     </div>
                     <div className='flex items-center justify-between pt-5'>
@@ -21,15 +50,7 @@ export default function AsidePirce() {
                             Descuento
                         </p>
                         <p className='text-base leading-none text-gray-800'>
-                            $30
-                        </p>
-                    </div>
-                    <div className='flex items-center justify-between pt-5'>
-                        <p className='text-base leading-none text-gray-800'>
-                            Impuesto
-                        </p>
-                        <p className='text-base leading-none text-gray-800'>
-                            $35
+                            {userCode}%
                         </p>
                     </div>
                 </div>
@@ -39,15 +60,14 @@ export default function AsidePirce() {
                             Total Final
                         </p>
                         <p className='text-2xl font-bold leading-normal text-right text-gray-800'>
-                            $10.000
-
+                            {isNaN(totalFinal) ? 0 : totalFinal.toLocaleString("es-AR", { style: "currency", currency: "ARS", minimumFractionDigits: 2 })}
                         </p>
                     </div>
                     <button
-                        onClick={() => setShow(!show)}
+                        onClick={() => sendOrder()}
                         className='text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white'
                     >
-                        Checkout
+                        Enviar Pedido
                     </button>
                 </div>
             </div>
