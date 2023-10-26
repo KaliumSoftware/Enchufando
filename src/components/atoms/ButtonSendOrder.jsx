37398bd14eb19a840e077e39b69395486a61d5b6
Valen <valenzalazar3@gmail.com>
2023-10-26 12:25:18 -0300

    feat(Order): post order in front

--- src/components/atoms/ButtonSendOrder.jsx
+++ src/components/atoms/ButtonSendOrder.jsx
@@ -1,33 +1,45 @@
 import React from 'react'
-
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '@/redux/slices/cartSlice';
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 export default function ButtonSendOrder({ totalFinal, allInCart }) {
-    const sendOrder = () => {
+    const dispatch = useDispatch();
+    const userId = useSelector((state) => state.user.loggedUser.id);
+    const handleSendOrder = async () => {
         if (isNaN(totalFinal)) {
-            alert('Hay productos incompletos, por favor seleccione todas las caracteristicas')
         } else {
-            if (confirm('Seguro que quiere enviar este pedido?')) {
-                const productsInOrder = allInCart.map((product) => {
-                    return {
-                        name: product.name,
-                        image: product.image,
-                        size: product.selectedSpec.size,
-                        price: product.selectedSpec.price,
-                        quantity: product.selectedSpec.quantity,
-                        pack: product.selectedSpec.pack,
-                        smallPack: product.selectedSpec.smallPack,
-                        bigPack: product.selectedSpec.bigPack
-                    }
-                })
-            } else {
-                alert('Pedido no enviado')
-            }
-
+            var productsInOrder = allInCart.map((product) => {
+                return {
+                    name: product.name,
+                    image: product.image,
+                    size: product.selectedSpec.size,
+                    price: product.selectedSpec.price,
+                    quantity: product.selectedSpec.quantity,
+                    pack: product.selectedSpec.pack,
+                    smallPack: product.selectedSpec.smallPack,
+                    bigPack: product.selectedSpec.bigPack
+                };
+            });
         }
-    }
+        try {
+            const { data } = await axios.post(`${apiUrl}/order`, {
+                products: productsInOrder,
+                totalPrice: totalFinal,
+                userId: userId
+            });
+            if (data.id) {
+                dispatch(clearCart());
+                alert('Pedido Enviado');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <button
-            onClick={() => sendOrder(totalFinal)}
+            onClick={() => handleSendOrder()}
             className='text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white'
         >
             Enviar Pedido
