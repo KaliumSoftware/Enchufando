ab29b91f99fc5bde8ae035dc378795002b8af676
Valen <valenzalazar3@gmail.com>
2023-10-26 12:25:21 -0300

    feat(Order): post order in front

--- src/components/organisms/ShoppingCart.jsx
+++ src/components/organisms/ShoppingCart.jsx
@@ -18,7 +18,7 @@ function ShoppingCart() {
       localStorage.getItem('cartProducts')
     );
 
-    if (productsOnLS.length) {
+    if (productsOnLS?.length) {
       dispatch(setCart(productsOnLS));
     }
   }, []);
@@ -30,7 +30,7 @@ function ShoppingCart() {
           type='button'
           onClick={() => setShow(!show)}
           className='relative inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-          // className='py-2 px-10 rounded bg-black hover:bg-indigo-600  text-white'
+        // className='py-2 px-10 rounded bg-black hover:bg-indigo-600  text-white'
         >
           {cartProducts?.length ? (
             <svg
