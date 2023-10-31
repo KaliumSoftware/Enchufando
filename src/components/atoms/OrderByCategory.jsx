870a94887439abceb13bc555105acd09f66c0843
DiazAdriel0 <diazadriel0@gmail.com>
2023-10-31 11:05:29 -0300

    feat(atoms/OrderByCategory): add select on mobile view

--- src/components/atoms/OrderByCategory.jsx
+++ src/components/atoms/OrderByCategory.jsx
@@ -1,17 +1,26 @@
+import { useState, useEffect } from 'react';
 import { filterByCategory } from '@/redux/slices/productSlice';
 import { useDispatch } from 'react-redux';
 import { category } from '@/../utils/consts';
 
 const OrderByCategory = () => {
+  const [selectValue, setSelectValue] = useState('Ver todas');
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (selectValue) {
+      dispatch(filterByCategory(selectValue));
+    }
+  }, [selectValue]);
 
   const handleCategory = (e) => {
     dispatch(filterByCategory(e.target.textContent));
   };
+
   return (
     <article>
-      <h3 className='mt-4 mb-2 text-xl'>Categorías</h3>
-      <ul className='flex flex-col gap-1'>
+      <h3 className='md:mt-4 mb-3 text-xl'>Categorías</h3>
+      <ul className='hidden md:flex flex-col gap-1'>
         {category.map((item) => (
           <li
             className='w-fit cursor-pointer'
@@ -23,6 +32,22 @@ const OrderByCategory = () => {
           </li>
         ))}
       </ul>
+
+      <select
+        className='none md:hidden'
+        onChange={(e) => setSelectValue(e.target.value)}
+        value={selectValue}
+      >
+        {category.map((item) => (
+          <option
+            className='w-fit cursor-pointer'
+            key={item}
+            value={item}
+          >
+            {item}
+          </option>
+        ))}
+      </select>
     </article>
   );
 };
