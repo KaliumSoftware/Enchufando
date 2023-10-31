f9a87c892b97ffb2b36a6cefb481e5a698e7f7bf
Joshua Candia <joshuacandia74@gmail.com>
2023-10-31 17:09:55 -0300

    feat(Server/Resend): emails templates created

--- server/src/utils/resend.js
+++ server/src/utils/resend.js
@@ -1,13 +1,15 @@
+const emailTemplate = require('./templates');
+
 const { Resend } = require('resend');
 const resend = new Resend('re_aZuWkNSF_Cqs53ZtAhbUmKWwPJoWSV4Qw');
 
 (async function () {
   try {
     const data = await resend.emails.send({
-      from: 'Acme <info.enchufando@resend.dev>',
+      from: 'Enchufando <info.enchufando@resend.dev>',
       to: ['joshuanicolas74@gmail.com'],
-      subject: 'Hola!',
-      html: '<strong>Funciona!</strong>'
+      subject: 'Nuevos Productos!',
+      html: emailTemplate
     });
 
     console.log(data);
