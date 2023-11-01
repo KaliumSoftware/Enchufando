94fb8dddb8847a9531d182cba3df99c09df33d03
Joshua Candia <joshuacandia74@gmail.com>
2023-11-01 14:36:16 -0300

    feat(Server/utils):nodemailer started

--- server/src/router/routes/index.js
+++ server/src/router/routes/index.js
@@ -2,10 +2,12 @@ const orderRouter = require('./orderRouter');
 const productRouter = require('./productRouter');
 const userRouter = require('./userRouter');
 const codesRouter = require('./codesRouter');
+const mailsRouter = require('./mailsRouter');
 
 module.exports = {
   orderRouter,
   productRouter,
   userRouter,
-  codesRouter
+  codesRouter,
+  mailsRouter
 };
