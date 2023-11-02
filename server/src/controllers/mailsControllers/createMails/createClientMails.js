e1086b34df7d2900bf14acc6e37299c174e6df66
Joshua Candia <joshuacandia74@gmail.com>
2023-11-02 17:00:34 -0300

    fix(Server/Nodemailer):nodemailer working

--- server/src/controllers/mailsControllers/createMails/createClientMails.js
+++ server/src/controllers/mailsControllers/createMails/createClientMails.js
@@ -5,31 +5,20 @@ const { newProducts } = require('../../../utils/templates');
 
 const createClientMails = async (firstName) => {
   try {
-    const mailOptions = {
-      from: `ENCHUFANDO ${process.env.EMAIL}`,
-      to: process.env.EMAIL,
-      subject: `Bienvenido ${firstName}`,
-      html: newProducts
-    };
-
-    const response = await mailSender(mailOptions);
-
     const allUsers = await User.findAll();
     if (allUsers.length === 0) {
       throw new Error('No hay usuarios');
     }
 
-    /*
-    const allUsersEmails = allUsers.map((user) => user.email);
-    for (let i = 0; i < allUsersEmails.length; i++) {
-      mailOptions = {
+    for (let i = 0; i < allUsers.length; i++) {
+      const mailOptions = {
         from: `ENCHUFANDO ${process.env.EMAIL}`,
-        to: 'joshuacandia74@gmail.com',
-        subject: `Bienvenido ${firstName}`,
+        to: allUsers[i].email,
+        subject: `Bienvenido ${allUsers[i].name}`,
         html: newProducts
       };
-    } 
-    */
+      mailSender(mailOptions);
+    }
   } catch (error) {
     console.error(error);
     return error;
