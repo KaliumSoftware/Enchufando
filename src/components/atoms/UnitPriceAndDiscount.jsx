d4cd5b4159e3622354ed73f1be27cad139d3e066
DiazAdriel0 <diazadriel0@gmail.com>
2023-11-02 11:58:05 -0300

    fix(atoms/UnitPrice): fix price with discount

--- src/components/atoms/UnitPriceAndDiscount.jsx
+++ src/components/atoms/UnitPriceAndDiscount.jsx
@@ -1,51 +1,62 @@
-import React from 'react'
+import React from 'react';
 import { setLoggedUser } from '@/redux/slices/userSlice';
 import { useSelector } from 'react-redux';
 
 export default function UnitPriceAndDiscount(props) {
-    const { id, localId, selectedSpec } = props;
-    const discount = useSelector((state) => state.user.loggedUser.discount);
+  const { id, localId, selectedSpec } = props;
+  const discount = useSelector(
+    (state) => state.user.loggedUser.discount
+  );
 
+  return (
+    <div className='flex justify-around'>
+      <div>
+        <p className='text-lg font-bold text-gray-500 dark:text-white line-through'>
+          S/ desc. $ {selectedSpec?.price}
+        </p>
+        <p className='text-lg font-bold text-gray-900 dark:text-white'>
+          C/ desc.{' '}
+          {selectedSpec?.price &&
+            discount &&
+            (
+              selectedSpec?.price -
+              selectedSpec?.price * discount
+            ).toLocaleString('es-AR', {
+              style: 'currency',
+              currency: 'ARS',
+              minimumFractionDigits: 2
+            })}
+        </p>
+      </div>
 
-    return (
-        <div className='flex justify-around'>
-            <div>
-                <p
-                    className={`text-lg font-bold text-gray-500 dark:text-white ${
-        /* user.discount &&  */ 'line-through'
-                        }`}
-                >
-                    S/ desc. $ {selectedSpec?.price}
-                </p>
-                <p className='text-lg font-bold text-gray-900 dark:text-white'>
-                    C/ desc. $
-                    {(selectedSpec?.pack === 'small' || selectedSpec?.pack === 'big') &&
-                        selectedSpec?.price && discount &&
-                        (selectedSpec?.price * discount)
-                    }
-
-                </p>
-            </div>
-
-            <div className='w-2/6'>
-                <p className='text-lg font-bold text-gray-900 dark:text-white'>
-                    Total Unidad
-                </p>
-                <p className='text-lg font-bold text-gray-900 dark:text-white'>
-                    {(selectedSpec?.pack === 'small' || selectedSpec?.pack === 'big') &&
-                        selectedSpec?.price &&
-                        selectedSpec?.quantity &&
-                        (
-                            selectedSpec?.price *
-                            (selectedSpec?.pack === 'small'
-                                ? selectedSpec?.smallPack
-                                : selectedSpec?.bigPack) * discount *
-                            selectedSpec?.quantity
-                        )?.toLocaleString("es-AR", { style: "currency", currency: "ARS", minimumFractionDigits: 2 })}
-                </p>
-            </div></div>
-    )
+      <div className='w-2/6'>
+        <p className='text-lg font-bold text-gray-900 dark:text-white'>
+          Total Unidad
+        </p>
+        <p className='text-lg font-bold text-gray-900 dark:text-white'>
+          {(selectedSpec?.pack === 'small' ||
+            selectedSpec?.pack === 'big') &&
+            selectedSpec?.price &&
+            selectedSpec?.quantity &&
+            (
+              selectedSpec?.price *
+                (selectedSpec?.pack === 'small'
+                  ? selectedSpec?.smallPack
+                  : selectedSpec?.bigPack) *
+                selectedSpec?.quantity -
+              selectedSpec?.price *
+                selectedSpec?.quantity *
+                (selectedSpec?.pack === 'small'
+                  ? selectedSpec?.smallPack
+                  : selectedSpec?.bigPack) *
+                discount
+            )?.toLocaleString('es-AR', {
+              style: 'currency',
+              currency: 'ARS',
+              minimumFractionDigits: 2
+            })}
+        </p>
+      </div>
+    </div>
+  );
 }
-
-
-
