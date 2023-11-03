8c8f939f3f93e2cb7f274ded4184102c3ef48c1c
Joshua Candia <joshuacandia74@gmail.com>
2023-11-03 17:07:28 -0300

    feat(Server/controllers): send client and admin mails

--- server/src/controllers/mailsControllers/createMails/createAdminMails.js
+++ server/src/controllers/mailsControllers/createMails/createAdminMails.js
@@ -1,2 +1,8 @@
-const createAdminMails = () => {};
+const mailSender = require('../../../utils/nodemailer');
+const { newProducts } = require('../../../utils/templates');
+
+const createAdminMails = () => {
+  try {
+  } catch (error) {}
+};
 module.exports = createAdminMails;
--- server/src/handlers/mailsHandlers/createMails/sendAdminMails.js
+++ server/src/handlers/mailsHandlers/createMails/sendAdminMails.js
@@ -2,7 +2,15 @@ const { createAdminMails } = require('../../../controllers');
 
 const sendAdminMails = () => {
   try {
-  } catch (error) {}
+    const response = createAdminMails();
+    if (response === 'Pedido cargado') {
+      res.status(200).send({ message: response });
+    } else {
+      throw new Error('Error al cargar el pedido');
+    }
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
 };
 
 module.exports = sendAdminMails;
