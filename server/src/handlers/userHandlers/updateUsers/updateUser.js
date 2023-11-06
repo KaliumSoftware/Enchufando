01e9fce6d1e5453f5bb678c03eb7773331e5259b
Joshua Candia <joshuacandia74@gmail.com>
2023-11-06 20:28:59 -0300

    feat(Server/Routes): console.logs removed

--- server/src/handlers/userHandlers/updateUsers/updateUser.js
+++ server/src/handlers/userHandlers/updateUsers/updateUser.js
@@ -3,30 +3,40 @@ const { updateUserById } = require('./../../../controllers');
 const updateUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const { name, email, password, address } = req.body;
+    const { name, email, password, address, isAdmin } = req.body;
 
-    const recibedProperties = { name, email, password, address };
-
+    const recibedProperties = {
+      name,
+      email,
+      password,
+      address,
+      isAdmin
+    };
+    console.log(isAdmin);
     const filledProperties = Object.entries(recibedProperties)
-      // eslint-disable-next-line no-unused-vars
       .filter(([_, value]) => {
         return (
           (typeof value === 'string' && value.trim() !== '') ||
           (Array.isArray(value) && value.length > 0) ||
-          typeof value === 'object'
+          typeof value === 'object' ||
+          typeof value === 'boolean'
         );
       })
       .map(([key, value]) => [key, value]);
 
     const filledObject = Object.fromEntries(filledProperties);
 
+    console.log(filledProperties);
+
     const user = await updateUserById(id, filledObject);
 
     if (!user) {
-      throw new Error('An error ocurred while updating the user');
+      throw new Error('el usuario no se pudo actualizar');
     }
 
-    return res.status(200).json('Data updated succesfully');
+    return res
+      .status(200)
+      .json('el usuario fue actualizado con exito');
   } catch (error) {
     console.error(error);
     return res.status(400).json({ error: error.message });
