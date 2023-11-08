d2920b592c7e0afb77165844667b3561a818e567
Valen <valenzalazar3@gmail.com>
2023-11-08 14:52:53 -0300

    fix(OrderBy/Category) fix style

--- src/components/atoms/OrderByType.jsx
+++ src/components/atoms/OrderByType.jsx
@@ -10,10 +10,10 @@ const OrderByType = () => {
   };
 
   return (
-    <article className='flex flex-col mr-6 md:mr-0 lg:flex-row gap-2'>
+    <article className='flex flex-col mr-6 md:mr-0 lg:flex-row gap-1'>
       <h3 className='mb-2 text-xl md:text-md'>Ordenar por</h3>
       <select
-        className='mr-3'
+        className='mr-3 focus:ring-2 focus:ring-blueDark font-medium rounded-lg py-2.5 inline-flex items-center'
         onChange={handleType}
         name=''
         id=''
