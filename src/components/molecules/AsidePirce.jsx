300fdeafa085c7e40a661f129e9f2c5f0c14a42a
Joshua Candia <joshuacandia74@gmail.com>
2023-11-09 19:48:21 -0300

    fix(Client/Cart): discount fixed

--- src/components/molecules/AsidePirce.jsx
+++ src/components/molecules/AsidePirce.jsx
@@ -4,10 +4,9 @@ import ButtonSendOrder from '../atoms/ButtonSendOrder';
 
 export default function AsidePirce() {
   const allInCart = useSelector((state) => state.cart.cartProducts);
-  const userCode = useSelector(
-    (state) => state.user.loggedUser.discount
-  );
+  const userDiscount = useSelector((state) => state.user.loggedUser.discount);
 
+  console.log(userDiscount);
   const subTotal = allInCart
     .map((product) => {
       if (product.selectedSpec) {
@@ -28,19 +27,14 @@ export default function AsidePirce() {
     })
     .reduce((a, b) => a + b, 0);
 
-  const totalFinal = subTotal - subTotal * userCode;
-
+  const totalFinal = subTotal - subTotal * userDiscount;
   return (
     <div className=' md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full'>
       <div className='flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto'>
         <div>
-          <p className='text-4xl font-black leading-9 text-gray-800'>
-            Resumen
-          </p>
+          <p className='text-4xl font-black leading-9 text-gray-800'>Resumen</p>
           <div className='flex items-center justify-between pt-16'>
-            <p className='text-base leading-none text-gray-800'>
-              Sub Total
-            </p>
+            <p className='text-base leading-none text-gray-800'>Sub Total</p>
             <p className='text-base leading-none text-gray-800'>
               {isNaN(subTotal)
                 ? '$ 0,00'
@@ -52,19 +46,15 @@ export default function AsidePirce() {
             </p>
           </div>
           <div className='flex items-center justify-between pt-5'>
+            <p className='text-base leading-none text-gray-800'>Descuento</p>
             <p className='text-base leading-none text-gray-800'>
-              Descuento
-            </p>
-            <p className='text-base leading-none text-gray-800'>
-              {userCode * 100}%
+              {userDiscount * 10000}%
             </p>
           </div>
         </div>
         <div>
           <div className='flex items-center pb-6 justify-between lg:pt-5 pt-20'>
-            <p className='text-2xl leading-normal text-gray-800'>
-              Total Final
-            </p>
+            <p className='text-2xl leading-normal text-gray-800'>Total Final</p>
             <p className='text-2xl font-bold leading-normal text-right text-gray-800'>
               {isNaN(totalFinal)
                 ? '$ 0,00'
