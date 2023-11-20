fa769d37ab71f3ffce0c40f72140f1d3765a012b
Joshua Candia <joshuacandia74@gmail.com>
2023-11-20 15:42:07 -0300

    fix(Server/middlewares) : auth admin improved

--- server/src/middlewares/authAdmin.js
+++ server/src/middlewares/authAdmin.js
@@ -4,8 +4,8 @@ module.exports = function authAdmin(handler) {
   return async (req, res) => {
     try {
       let userId = 'notLogged';
-      if (req.body.userId !== undefined) userId = req.body.userId;
-      else if (req.query.userId !== undefined) userId = req.query.userId;
+      if (req.query.userId !== undefined) userId = req.query.userId;
+      else if (req.body.userId !== undefined) userId = req.body.userId;
       else if (req.params.userId !== undefined) userId = req.params.userId;
 
       if (userId === 'notLogged') {
