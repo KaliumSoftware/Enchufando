ae70c661627e302b1c98328de41a4485e4a8e6a2
Joshua Candia <joshuacandia74@gmail.com>
2023-11-20 15:08:52 -0300

    fix(Client/Filters) : filters changed (not finished)

--- src/components/atoms/Quantity.jsx
+++ src/components/atoms/Quantity.jsx
@@ -1,59 +1,59 @@
-import React from 'react'
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setSpecificationsCart } from '@/redux/slices/cartSlice';
 
-
 export default function Quantity(props) {
+  const dispatch = useDispatch();
+  const { id, localId, selectedSpec } = props;
 
-    const dispatch = useDispatch()
-    const { id, localId, selectedSpec } = props;
+  const handleQuantityChange = (change) => {
+    if (!selectedSpec.quantity) {
+      dispatch(
+        setSpecificationsCart({
+          selectedSpec: { ...selectedSpec, quantity: 1 },
+          localId
+        })
+      );
+    } else {
+      const newQuantity = selectedSpec?.quantity + change;
+      const value = Number(newQuantity);
+      if (value >= 1) {
+        dispatch(
+          setSpecificationsCart({
+            selectedSpec: { ...selectedSpec, quantity: newQuantity },
+            localId
+          })
+        );
+      }
+    }
+  };
 
-    const handleQuantityChange = (change) => {
-        if (!selectedSpec.quantity) {
-            dispatch(setSpecificationsCart({
-                selectedSpec: { ...selectedSpec, quantity: 1 },
-                localId
-            }))
-        } else {
-            const newQuantity = selectedSpec?.quantity + change;
-            const value = Number(newQuantity);
-            if (value >= 1) {
-                dispatch(setSpecificationsCart({
-                    selectedSpec: { ...selectedSpec, quantity: newQuantity },
-                    localId
-                }))
-            }
-        }
-
-    };
-    console.log(selectedSpec)
-    const isQuantityEnabled = !!selectedSpec?.pack;
-    return (
-        <div>
-            <div className="flex">
-                <p className="text-sm text-black mt-3">
-                    Cantidad: {selectedSpec?.quantity}
-                </p>
-                <button
-                    className="mt-3 mb-5 ml-2 w-10 rounded-md bg-gray-900 px-0.5 py-.5 font-medium text-white"
-                    onClick={() => handleQuantityChange(1)}
-                    disabled={!isQuantityEnabled}
-                >
-                    +
-                </button>
-                <button
-                    className="mt-3 mb-5 ml-2 w-10 rounded-md bg-gray-900 px-0.5 py-.5 font-medium text-white"
-                    onClick={() => handleQuantityChange(-1)}
-                    disabled={!isQuantityEnabled || selectedSpec?.quantity <= 1}
-                >
-                    -
-                </button>
-            </div>
-        </div>
-    )
+  const isQuantityEnabled = !!selectedSpec?.pack;
+  return (
+    <div>
+      <div className='flex'>
+        <p className='text-sm text-black mt-3'>Cantidad: {selectedSpec?.quantity}</p>
+        <button
+          className='mt-3 mb-5 ml-2 w-10 rounded-md bg-gray-900 px-0.5 py-.5 font-medium text-white'
+          onClick={() => handleQuantityChange(1)}
+          disabled={!isQuantityEnabled}
+        >
+          +
+        </button>
+        <button
+          className='mt-3 mb-5 ml-2 w-10 rounded-md bg-gray-900 px-0.5 py-.5 font-medium text-white'
+          onClick={() => handleQuantityChange(-1)}
+          disabled={!isQuantityEnabled || selectedSpec?.quantity <= 1}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
 }
 
-{/* <div>
+{
+  /* <div>
                 <label
                     htmlFor={`${localId}quantity`}
                     className='relative block overflow-hidden rounded-md border border-gray-200 px-9 pt-3 shadow-sm focus-within:border-black focus-within:ring-1 focus-within:ring-black selection:'
@@ -73,4 +73,5 @@ export default function Quantity(props) {
                         Cantidad
                     </span>
                 </label>
-            </div> */}
+            </div> */
+}
