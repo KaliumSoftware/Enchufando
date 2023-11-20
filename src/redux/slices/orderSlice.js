c377780d9bc52be31149539438dbbb93fb9584d6
Joshua Candia <joshuacandia74@gmail.com>
2023-11-20 15:06:40 -0300

    feat(Client/redux): recent orders added

--- src/redux/slices/orderSlice.js
+++ src/redux/slices/orderSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { differenceInWeeks, parseISO } from 'date-fns';
 
 const initialState = {
   orderProducts: [],
-  allUserOrders: []
+  allUserOrders: [],
+  recentOrders: []
 };
 
 const orderSlice = createSlice({
@@ -14,9 +16,16 @@ const orderSlice = createSlice({
     },
     setAllUserOrders: (state, action) => {
       state.allUserOrders = action.payload;
+    },
+    setRecentOrders: (state, action) => {
+      state.recentOrders = action.payload.filter((order) => {
+        const orderDate = parseISO(order.createdAt);
+        const weeksDifference = differenceInWeeks(new Date(), orderDate);
+        return weeksDifference <= 3;
+      });
     }
   }
 });
 
-export const { setOrder, setAllUserOrders } = orderSlice.actions;
+export const { setOrder, setAllUserOrders, setRecentOrders } = orderSlice.actions;
 export default orderSlice.reducer;
